2bcb164e5506d989c7f06f726bb3e3424cf94e29
Jose Martins <josemartins90@gmail.com>
2020-07-07 18:24:32 +0100

    add gicv3 support

--- src/arch/armv8/sources.mk
+++ src/arch/armv8/sources.mk
@@ -1,2 +1,8 @@
-arch_c_srcs:= init.c psci.c gicv2.c
+arch_c_srcs:= init.c psci.c
 arch_s_srcs:= exceptions.S page_tables.S start.S
+
+ifeq ($(GIC_VERSION),GICV3)
+	arch_c_srcs+=gicv3.c
+else
+	arch_c_srcs+=gicv2.c
+endif
