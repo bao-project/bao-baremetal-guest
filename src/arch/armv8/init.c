67d1d4169ef12e06ba020eab3dd7635d49a3605f
Jose Martins <josemartins90@gmail.com>
2020-08-11 15:10:11 +0100

    armv8: probe all available cores to wake up

--- src/arch/armv8/init.c
+++ src/arch/armv8/init.c
@@ -8,9 +8,11 @@ void _start();
 void arch_init(uint64_t cpuid){
     gic_init();
     if(cpuid == 0){
-        for(int i = 1; i < 4; i++){
-            psci_cpu_on(i, (uintptr_t) _start, 0);
-        }
+        size_t i = 1;
+        int ret;
+        do {
+            ret = psci_cpu_on(i, (uintptr_t) _start, 0);
+        } while(i++, ret == PSCI_E_SUCCESS);
     }
     asm volatile("MSR   DAIFClr, #2\n\t");
 }
