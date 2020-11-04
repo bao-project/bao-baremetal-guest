d2f04eb0158a9fe7a194a4310869840731d6d8bd
Jose Martins <josemartins90@gmail.com>
2020-11-04 22:04:06 +0000

    increase zcu and firesim platforms memory

--- src/platform/rocket-firesim/inc/plat.h
+++ src/platform/rocket-firesim/inc/plat.h
@@ -2,7 +2,7 @@
 #define PLAT_H
 
 #define PLAT_MEM_BASE 0x80200000
-#define PLAT_MEM_SIZE 0x00100000
+#define PLAT_MEM_SIZE 0x08000000
 
 #define PLAT_TIMER_FREQ (1000000ull) //1 MHz
 #define UART_IRQ_ID (0)
--- src/platform/rocket-fpga-zcu/inc/plat.h
+++ src/platform/rocket-fpga-zcu/inc/plat.h
@@ -2,7 +2,7 @@
 #define PLAT_H
 
 #define PLAT_MEM_BASE 0x40200000
-#define PLAT_MEM_SIZE 0x01000000
+#define PLAT_MEM_SIZE 0x08000000
 
 #define PLAT_TIMER_FREQ (1000000ull) //1 MHz
 #define UART_IRQ_ID (1)
