11362abf581328ce7e4a27ead5d5dcb4eb1b379b
Jose Martins <josemartins90@gmail.com>
2022-06-14 10:33:04 +0100

    fix(timer): timer macro base types
    If not forcing unsigned long as the base type, these macros could
    overflow for large time values.
    
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/core/inc/timer.h
+++ src/core/inc/timer.h
@@ -4,8 +4,8 @@
 #include <arch/timer.h>
 
 #define TIME_US(us) ((TIMER_FREQ)*(us)/(1000000ull))
-#define TIME_MS(ms) (TIME_US((ms)*1000))
-#define TIME_S(s)   (TIME_MS((s)*1000))
+#define TIME_MS(ms) (TIME_US((ms)*1000ull))
+#define TIME_S(s)   (TIME_MS((s)*1000ull))
 
 uint64_t timer_get();
 void timer_set(uint64_t n);
