bbcccfaabf8d0e71dd0db3c1fb727d52fcb87b42
Jose Martins <josemartins90@gmail.com>
2022-06-14 13:27:55 +0100

    feat(plat/fvp-r): add support for cortex-r based fvp platforms
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

---
+++ src/platform/fvp-r/inc/plat.h
@@ -0,0 +1,14 @@
+#ifndef PLAT_H
+#define PLAT_H
+
+#define PLAT_MEM_BASE 0x0
+#define PLAT_MEM_SIZE 0x8000000
+
+#define PLAT_GENERIC_TIMER_CNTCTL_BASE  (0xaa430000ull)
+
+#define PLAT_GICD_BASE_ADDR (0xAF000000)
+#define PLAT_GICR_BASE_ADDR (0xAF100000)
+
+#define UART_IRQ_ID 37
+
+#endif
