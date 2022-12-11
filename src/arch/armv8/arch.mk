8daa246129b063c5b9a8da422cfd5b52429bae93
Jose Martins <josemartins90@gmail.com>
2022-12-11 15:45:27 +0000

    feat(armv8/aarch32): enable floating point

--- src/arch/armv8/arch.mk
+++ src/arch/armv8/arch.mk
@@ -1,15 +1,15 @@
-ARM_PROFILE?=armv8-a
-
-ARCH_GENERIC_FLAGS =-DGIC_VERSION=$(GIC_VERSION) -march=$(ARM_PROFILE)
-ARCH_ASFLAGS = 
-ARCH_CFLAGS = -mgeneral-regs-only
-ARCH_CPPFLAGS =	
-ARCH_LDFLAGS = 
 
 ARCH_SUB?=aarch64
 arch_sub_dir:=$(cur_dir)/$(ARCH_SUB)
 include $(arch_sub_dir)/arch_sub.mk
 SRC_DIRS+=$(arch_sub_dir)
 INC_DIRS+=$(arch_sub_dir)/inc
-
 include $(arch_sub_dir)/sources.mk
+
+ARM_PROFILE?=armv8-a
+ARCH_GENERIC_FLAGS +=-DGIC_VERSION=$(GIC_VERSION) -march=$(ARM_PROFILE) \
+	$(ARCH_SUB_GENERIC_FLAGS)
+ARCH_ASFLAGS = 
+ARCH_CFLAGS = -mgeneral-regs-only
+ARCH_CPPFLAGS =	
+ARCH_LDFLAGS = 
