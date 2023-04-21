f384f846ededc0da4dff86231e8ad2d97c363bc1
Jose Martins <josemartins90@gmail.com>
2023-04-21 13:49:39 +0100

    fix(rpi4): uart address works for both the platform's and std address spaces
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/platform/rpi4/inc/plat.h
+++ src/platform/rpi4/inc/plat.h
@@ -7,7 +7,7 @@
 #define PLAT_GICD_BASE_ADDR (0xff841000)
 #define PLAT_GICC_BASE_ADDR (0xff842000)
 
-#define PLAT_UART_ADDR (0xfe215040)
+#define PLAT_UART_ADDR (0xfe215000)
 #define UART_IRQ_ID (125)
 
 #endif
