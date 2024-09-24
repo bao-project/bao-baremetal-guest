c4dd14ba2cfd8e2c8e58ca26ce8bbe1ade7b1201
David Cerdeira <davidmcerdeira@gmail.com>
2024-09-24 15:01:07 +0100

    feat(platform/imx8mp-verdin): add platform
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

---
+++ src/platform/imx8mp-verdin/imx8mp.c
@@ -0,0 +1,34 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#include <plat.h>
+#include <imx_uart.h>
+
+volatile struct imx_uart* const uart = (volatile struct imx_uart*)UART_ADDR;
+
+void uart_init()
+{
+    imx_uart_init(uart);
+}
+
+void uart_putc(char c)
+{
+    imx_uart_putc(uart, c);
+}
+
+char uart_getchar()
+{
+    return imx_uart_getchar(uart);
+}
+
+void uart_enable_rxirq()
+{
+    imx_uart_enable_rxirq(uart);
+}
+
+void uart_clear_rxirq()
+{
+    imx_uart_clear_rxirq(uart);
+}
