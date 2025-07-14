7fea108dfb3b7ba2fe29081b7943d32d82a7c1ce
Jose Martins <josemartins90@gmail.com>
2025-07-14 20:55:34 +0100

    feat(riscv): use compressed instruction set
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/riscv/arch.mk
+++ src/arch/riscv/arch.mk
@@ -2,12 +2,12 @@ ARCH_SUB?=riscv64
 
 ifeq ($(ARCH_SUB), riscv64)
 CROSS_COMPILE ?= riscv64-unknown-elf-
-riscv_march:=rv64g
+riscv_march:=rv64gc
 riscv_abi:=lp64d
 else ifeq ($(ARCH_SUB), riscv32)
 CROSS_COMPILE ?= riscv32-unknown-elf-
-riscv_march:=rv32g
-riscv_abi:=ilp32d
+riscv_march:=rv32gc
+riscv_abi:=ilp32f
 else
 $(error RISC-V $(ARCH_SUB) not supported!)
 endif
