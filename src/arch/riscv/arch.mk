d6cabe8405c6f7f799fa2257aa847b00d69cc58c
Jose Martins <josemartins90@gmail.com>
2025-11-18 13:41:19 +0000

    fix(riscv): specify extensions instead of using g
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/riscv/arch.mk
+++ src/arch/riscv/arch.mk
@@ -2,11 +2,11 @@ ARCH_SUB?=riscv64
 
 ifeq ($(ARCH_SUB), riscv64)
 CROSS_COMPILE ?= riscv64-unknown-elf-
-riscv_march:=rv64gc
+riscv_march:=rv64imafdc_zicsr
 riscv_abi:=lp64d
 else ifeq ($(ARCH_SUB), riscv32)
 CROSS_COMPILE ?= riscv32-unknown-elf-
-riscv_march:=rv32gc
+riscv_march:=rv32imafc_zicsr
 riscv_abi:=ilp32f
 else
 $(error RISC-V $(ARCH_SUB) not supported!)
