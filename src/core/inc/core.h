4dc3c1ba763828300bc1a135e43422c8a2100d02
David Cerdeira <davidmcerdeira@gmail.com>
2026-02-04 21:03:56 +0000

    feat(make): add non-unified memory model
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/core/inc/core.h
+++ src/core/inc/core.h
@@ -11,8 +11,8 @@
 #endif /* __ASSEMBLER__ */
 
 #ifdef STD_ADDR_SPACE
-#ifdef MPU
-#error Cannot provide STD_ADDR_SPACE for MPU systems
+#if defined(MPU) || defined(MEM_NON_UNIFIED)
+#error Cannot provide STD_ADDR_SPACE for MPU or MEM_NON_UNIFIED systems
 #endif
 #if defined(MEM_BASE) || defined(MEM_SIZE)
 #error Cannot define MEM_BASE or MEM_SIZE for STD_ADDR_SPACE
@@ -30,8 +30,41 @@
 #define MEM_SIZE PLAT_MEM_SIZE
 #endif
 
-#define UART_ADDR PLAT_UART_ADDR
+#ifndef RO_MEM_BASE
+#define RO_MEM_BASE PLAT_RO_MEM_BASE
+#endif
 
+#ifndef RO_MEM_SIZE
+#define RO_MEM_SIZE PLAT_RO_MEM_SIZE
+#endif
+
+#ifndef RW_MEM_BASE
+#define RW_MEM_BASE PLAT_RW_MEM_BASE
+#endif
+
+#ifndef RW_MEM_SIZE
+#define RW_MEM_SIZE PLAT_RW_MEM_SIZE
+#endif
+
+#ifndef STACKHEAP_SIZE
+#define STACKHEAP_SIZE PLAT_STACKHEAP_SIZE
+#endif
+
+#define UART_ADDR PLAT_UART_ADDR
+#endif
+
+#ifdef MEM_NON_UNIFIED
+#if !defined(RO_MEM_BASE) || !defined(RO_MEM_SIZE) || !defined(RW_MEM_BASE) || !defined(RW_MEM_SIZE)
+  #error "Non-unified model requires RO_MEM_BASE/RO_MEM_SIZE and RW_MEM_BASE/RW_MEM_SIZE."
+#endif
+
+#if defined(MEM_BASE) || defined(MEM_SIZE)
+  #error "Non-unified model must not define MEM_BASE/MEM_SIZE."
+#endif
+#else /* unified */
+#if !defined(MEM_BASE) || !defined(MEM_SIZE)
+  #error "Unified model requires MEM_BASE and MEM_SIZE."
+#endif
 #endif
 
 #ifndef SHMEM_BASE
