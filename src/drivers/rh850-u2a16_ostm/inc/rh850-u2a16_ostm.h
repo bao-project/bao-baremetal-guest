61c8b2c92695fafcbcd5a46cfe57bcbabd749e7d
David Cerdeira <davidmcerdeira@gmail.com>
2026-02-04 20:15:37 +0000

    ref(850): mov ostm to header only implementation
    This improves timer access performance
    
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/drivers/rh850-u2a16_ostm/inc/rh850-u2a16_ostm.h
+++ src/drivers/rh850-u2a16_ostm/inc/rh850-u2a16_ostm.h
@@ -23,8 +23,25 @@ struct rh850_u2a16_OSTMn {
     // volatile uint16_t IC0CKSEL9;
 };
 
-void rh850_u2a16_OSTMn_enable(struct rh850_u2a16_OSTMn* timer);
-uint64_t rh850_u2a16_OSTMn_get(struct rh850_u2a16_OSTMn* timer);
-void rh850_u2a16_OSTMn_set(struct rh850_u2a16_OSTMn* timer, uint64_t n);
+static inline void rh850_u2a16_OSTMn_enable(struct rh850_u2a16_OSTMn* timer) {
+    timer->OSTMnCTL |= (0b00000010); // Set to Free-run compare mode
+    timer->OSTMnCTL &= (0b11111110); // Disable interrupts when counting starts, we don't need it
+    timer->OSTMnCTL &= (0b11111011); // Set OSTMnMD2 to 0, it will reset CNT to 0x0 when start.
 
+    // Enable interrupt
+    timer->OSTMnCTL |= (0b10000000);
+}
+
+static inline uint64_t rh850_u2a16_OSTMn_get(struct rh850_u2a16_OSTMn* timer) {
+    return timer->OSTMnCNT;
+}
+
+static inline void rh850_u2a16_OSTMn_set(struct rh850_u2a16_OSTMn* timer, uint64_t n) {
+    // Stop
+    timer->OSTMnTT = 0x01;
+    // Reset CMP, CNT will reset to 0
+    timer->OSTMnCMP = n;
+    // Start
+    timer->OSTMnTS |= (0x01);
+}
 #endif /* RH850_U2A16_OSTM_H */
--- src/drivers/rh850-u2a16_ostm/sources.mk
+++
@@ -1,2 +0,0 @@
-driver_c_srcs+=rh850-u2a16_ostm/rh850-u2a16_ostm.c
-driver_s_srcs+=
