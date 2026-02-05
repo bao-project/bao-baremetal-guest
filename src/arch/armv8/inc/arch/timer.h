fcc19d4b91846197255050715b4c9bdb4a889428
Daniel Oliveira <drawnpoetry@gmail.com>
2026-02-05 15:25:44 +0000

    fix(timer): remove timer enable from arch init
    Timer interrupt should be masked during init to avoid any unwanted
    timer interrupt from unmasking all interrupts. The timer_enable
    api is responsible for enabling and unmasking the timer interrupt
    
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/arch/armv8/inc/arch/timer.h
+++ src/arch/armv8/inc/arch/timer.h
@@ -6,9 +6,27 @@
 
 extern unsigned long TIMER_FREQ;
 
+#define CNTV_CTL_ENABLE (1 << 0)
+#define CNTV_CTL_IMASK  (1 << 1)
+
 static inline unsigned long timer_get_freq(void)
 {
     return sysreg_cntfrq_el0_read();
+}
+
+static inline void timer_int_en(bool en)
+{
+    if (en) {
+        sysreg_cntv_ctl_el0_write(sysreg_cntv_ctl_el0_read() & ~CNTV_CTL_IMASK);
+    } else {
+        sysreg_cntv_ctl_el0_write(sysreg_cntv_ctl_el0_read() | CNTV_CTL_IMASK);
+    }
+}
+
+static inline void timer_enable(void)
+{
+    timer_int_en(true);
+    sysreg_cntv_ctl_el0_write(CNTV_CTL_ENABLE);
 }
 
 static inline void timer_set(uint64_t n)
