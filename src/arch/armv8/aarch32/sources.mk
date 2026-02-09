356bcd1e8ce456ad76445bcf689238058ad55c29
Daniel Oliveira <drawnpoetry@gmail.com>
2026-02-09 13:32:28 +0000

    fix(aarch32): conditionally compile page_tables
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/arch/armv8/aarch32/sources.mk
+++ src/arch/armv8/aarch32/sources.mk
@@ -1,2 +1,5 @@
 arch_c_srcs:=
-arch_s_srcs+=$(addprefix $(ARCH_SUB)/, exceptions.S page_tables.S start.S)
+arch_s_srcs+=$(addprefix $(ARCH_SUB)/, exceptions.S start.S)
+ifndef MPU
+arch_s_srcs+=$(addprefix $(ARCH_SUB)/, page_tables.S)
+endif
