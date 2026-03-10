f65badd9c51cf18e23cb563d9567ce18f3cf7ed3
Diogo Costa <diogoandreveigacosta@gmail.com>
2026-03-10 09:10:39 +0000

    feat(bao_hypercall_api): add support to armv8
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

---
+++ src/arch/armv8/inc/arch/bao.h
@@ -0,0 +1,27 @@
+#ifndef __ARCH_BAO_H__
+#define __ARCH_BAO_H__
+
+#include <stdint.h>
+
+
+static inline void bao_hypercall(unsigned long id)
+{
+    register unsigned long r6 asm("r6") = id;
+    __asm__ volatile ("hvtrap 0" : : "r"(r6));
+}
+
+#define SMCC32_FID_VND_HYP_SRVC 0x86000000
+
+static uint64_t send_hypercall(uint64_t hc_id, uint64_t arg1, uint64_t arg2, uint64_t arg3) {
+    register uint64_t r0 asm("x0") = SMCC32_FID_VND_HYP_SRVC | hc_id;
+    register uint64_t r1 asm("x1") = arg1;
+    register uint64_t r2 asm("x2") = arg2;
+    register uint64_t r3 asm("x3") = arg3;
+
+    asm volatile("hvc   #0\n"
+                : "=r"(r0)
+                : "r"(r0), "r"(r1), "r"(r2), "r"(r3));
+    return r0;
+}
+
+#endif /* __ARCH_BAO_H__ */
---
+++ src/core/inc/bao.h
@@ -0,0 +1,9 @@
+#ifndef __BAO_H__
+#define __BAO_H__
+
+#include <arch/bao.h>
+
+#define BAO_YIELD_HYPCALL_ID  3
+#define BAO_CACHE_FLUSH_HYPCALL_ID  4
+
+#endif /* __BAO_H__ */
