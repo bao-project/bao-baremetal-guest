39b1516f357ae07b5e4fea3940990bffc70372a2
Daniel Oliveira <drawnpoetry@gmail.com>
2026-03-26 14:05:52 +0000

    feat(hypercall): add support for tricore
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/arch/tricore/inc/arch/hypercall.h
@@ -0,0 +1,16 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef __ARCH_HYPERCALL_H__
+#define __ARCH_HYPERCALL_H__
+
+static inline uint32_t bao_hypercall(unsigned long id)
+{
+    NOT_IMPLEMENTED();
+
+    return 0;
+}
+
+#endif /* __ARCH_HYPERCALL_H__ */
