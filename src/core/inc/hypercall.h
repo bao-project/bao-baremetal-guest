df38c2769e2b7d834dba2ba4dadce303b7ab1b00
Daniel Oliveira <drawnpoetry@gmail.com>
2026-03-26 14:04:55 +0000

    feat(hypercall): add bao hypercall ids
    These hypercalls are used in the context of the test framework.
    
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/core/inc/hypercall.h
@@ -0,0 +1,14 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef HYPERCALL_H
+#define HYPERCALL_H
+
+#include <arch/hypercall.h>
+
+#define BAO_YIELD_HYPCALL_ID       3
+#define BAO_CACHE_FLUSH_HYPCALL_ID 4
+
+#endif /* HYPERCALL_H */
