b41c8c76772b3b2aa824c4c983d15f9c09559b9d
leon <uomleon@163.com>
2026-04-10 17:41:57 +0800

    fix(e3650): add required MPU region descriptors
    Signed-off-by: leon <uomleon@163.com>

--- src/platform/e3650/e3650.c
+++ src/platform/e3650/e3650.c
@@ -4,8 +4,15 @@
  */
 
 #include <e3650_uart.h>
+#include <mem.h>
 #include <plat.h>
 #include <uart.h>
+
+const struct mpu_region plat_mpu_regs[] = {
+    MPU_REGION_DESC(0, 0x80000000, PRBAR_SH_IS, PRBAR_AP_RW_ALL, PRBAR_ATTR_NORMAL),
+    MPU_REGION_DESC(0x80000000, 0x80000000, PRBAR_SH_IS, PRBAR_AP_RW_ALL, PRBAR_ATTR_DEVICE),
+};
+DEFINE_PLAT_MPU_NUM_REGS();
 
 /* Global uart structure definition */
 volatile struct e3650_uart* const uart = (void*)PLAT_UART_ADDR;
