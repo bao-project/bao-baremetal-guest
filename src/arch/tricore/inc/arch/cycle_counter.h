f620a131c9200953c89f204896c31a2e59ba3ad1
Miguel Silva <miguelafsilva5@gmail.com>
2026-05-11 16:20:01 +0100

    feat(cc): clock cycle counter implementation for tc4
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- src/arch/tricore/inc/arch/cycle_counter.h
+++ src/arch/tricore/inc/arch/cycle_counter.h
@@ -8,9 +8,25 @@
 
 static inline uint64_t arch_cc_get_count(void)
 {
-    NOT_IMPLEMENTED();
-    return 0;
+    uint32_t cycles;
+    __asm__ volatile("mfcr %0, 0xFC04" // Read from CPU_CCNT register
+                     : "=d"(cycles));
+
+    return cycles;
 }
+
+static inline void arch_cc_start(void)
+{
+    uint32_t val = 0x2;
+    __asm__ volatile("mtcr 0xFC00, %0" ::"r"(val) : "memory");
+}
+
+static inline void arch_cc_stop(void)
+{
+    uint32_t val = 0;
+    __asm__ volatile("mtcr 0xFC00, %0" ::"r"(val) : "memory");
+}
+
 
 static inline void arch_cc_enable(void)
 {
@@ -19,17 +35,10 @@ static inline void arch_cc_enable(void)
 
 static inline void arch_cc_reset_count(void)
 {
-    NOT_IMPLEMENTED();
-}
-
-static inline void arch_cc_start(void)
-{
-    NOT_IMPLEMENTED();
-}
-
-static inline void arch_cc_stop(void)
-{
-    NOT_IMPLEMENTED();
+    arch_cc_stop();
+    uint32_t cycles = 0;
+    __asm__ volatile("mtcr 0xFC04, %0" ::"r"(cycles) : "memory");
+    arch_cc_start();
 }
 
 #endif /* ARCH_CYCLE_COUNTER_H_ */
