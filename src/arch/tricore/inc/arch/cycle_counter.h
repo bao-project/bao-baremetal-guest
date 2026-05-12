9897604c90d79aba8e17ccfc041b6ab2430d4e81
Miguel Silva <miguelafsilva5@gmail.com>
2026-05-12 10:43:16 +0100

    feat(csfr): update csfr access to use macros
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- src/arch/tricore/inc/arch/cycle_counter.h
+++ src/arch/tricore/inc/arch/cycle_counter.h
@@ -6,27 +6,22 @@
 #ifndef ARCH_CYCLE_COUNTER_H_
 #define ARCH_CYCLE_COUNTER_H_
 
+#include <arch/csfrs.h>
+
 static inline uint64_t arch_cc_get_count(void)
 {
-    uint32_t cycles;
-    __asm__ volatile("mfcr %0, 0xFC04" // Read from CPU_CCNT register
-                     : "=d"(cycles));
-
-    return cycles;
+    return csfr_ccnt_read();
 }
 
 static inline void arch_cc_start(void)
 {
-    uint32_t val = 0x2;
-    __asm__ volatile("mtcr 0xFC00, %0" ::"r"(val) : "memory");
+    csfr_cctrl_write(0x2);
 }
 
 static inline void arch_cc_stop(void)
 {
-    uint32_t val = 0;
-    __asm__ volatile("mtcr 0xFC00, %0" ::"r"(val) : "memory");
+    csfr_cctrl_write(0);
 }
-
 
 static inline void arch_cc_enable(void)
 {
@@ -36,8 +31,7 @@ static inline void arch_cc_enable(void)
 static inline void arch_cc_reset_count(void)
 {
     arch_cc_stop();
-    uint32_t cycles = 0;
-    __asm__ volatile("mtcr 0xFC04, %0" ::"r"(cycles) : "memory");
+    csfr_ccnt_write(0);
     arch_cc_start();
 }
 
