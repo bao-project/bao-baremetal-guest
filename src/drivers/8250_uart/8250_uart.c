d6380fbb5fe99982942afa47af7384a3c7725533
Jose Martins <josemartins90@gmail.com>
2026-06-06 03:22:37 +0100

    feat(drivers/8250): support XScale/PXA-style 16550 UARTs
    The SpacemiT K3 UART is a 16550 with PXA/XScale quirks. Raise MCR OUT2 -
    on a 16550 OUT2 gates the tri-state INTR output buffer, so without it the
    RX interrupt pends internally but never reaches the external INTR pin
    (Linux does the same in serial8250_init_mctrl). On platforms that define
    UART8250_XSCALE in their plat.h, keep the IER UART Unit Enable (UUE) bit
    set and enable the receiver time-out interrupt (RTOIE) along with RX.
    Also enable+clear the FIFOs, skip the divisor reprogram (the firmware
    already set the baud), and parenthesize the readl/writel macro args.
    
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/drivers/8250_uart/8250_uart.c
+++ src/drivers/8250_uart/8250_uart.c
@@ -6,13 +6,14 @@
 // #include <sbi/riscv_io.h>
 // #include <sbi_utils/serial/uart8250.h>
 #include <8250_uart.h>
+#include <plat.h>
 
 #define readb(addr)       (*((volatile uint8_t*)addr))
 #define writeb(val, addr) (*(volatile uint8_t*)(addr) = val)
 #define readw(addr)       (*((volatile uint16_t*)addr))
 #define writew(val, addr) (*(volatile uint16_t*)(addr) = val)
-#define readl(addr)       (*((volatile uint32_t*)addr))
-#define writel(val, addr) (*((volatile uint32_t*)addr) = val)
+#define readl(addr)       (*((volatile uint32_t*)(addr)))
+#define writel(val, addr) (*((volatile uint32_t*)(addr)) = val)
 
 /* clang-format off */
 
@@ -39,6 +40,16 @@
 #define UART_LSR_OE		0x02    /* Overrun error indicator */
 #define UART_LSR_DR		0x01    /* Receiver data ready */
 #define UART_LSR_BRK_ERROR_BITS	0x1E    /* BI, FE, PE, OE bits */
+
+#define UART_IER_ERBFI		0x01    /* Enable received data available interrupt */
+#define UART_IER_RTOIE		0x10    /* XScale: receiver time out interrupt enable */
+#define UART_IER_UUE		0x40    /* XScale: UART unit enable */
+
+#define UART_FCR_EN		0x01    /* Enable FIFOs */
+#define UART_FCR_RX_CLR		0x02    /* Clear receive FIFO */
+#define UART_FCR_TX_CLR		0x04    /* Clear transmit FIFO */
+
+#define UART_MCR_OUT2		0x08    /* OUT2: gates the INTR output buffer */
 
 /* clang-format on */
 
@@ -97,7 +108,13 @@ void uart8250_interrupt_handler()
 
 void uart8250_enable_rx_int()
 {
-    set_reg(UART_IER_OFFSET, 1);
+#ifdef UART8250_XSCALE
+    /* RTOIE must also be set so received data below the FIFO trigger level
+     * (re)raises the interrupt after a character timeout. */
+    set_reg(UART_IER_OFFSET, UART_IER_UUE | UART_IER_RTOIE | UART_IER_ERBFI);
+#else
+    set_reg(UART_IER_OFFSET, UART_IER_ERBFI);
+#endif
 }
 
 int uart8250_init(unsigned long base, u32 in_freq, u32 baudrate, u32 reg_shift, u32 reg_width)
@@ -113,19 +130,29 @@ int uart8250_init(unsigned long base, u32 in_freq, u32 baudrate, u32 reg_shift, 
     bdiv = uart8250_in_freq / (16 * uart8250_baudrate);
 
     /* Disable all interrupts */
+#ifdef UART8250_XSCALE
+    /* XScale/PXA-style ports gate the whole UART on the IER Unit Enable (UUE)
+     * bit, so keep it set while masking all interrupt sources. */
+    set_reg(UART_IER_OFFSET, UART_IER_UUE);
+#else
     set_reg(UART_IER_OFFSET, 0x00);
+#endif
     /* Enable DLAB */
-    set_reg(UART_LCR_OFFSET, 0x80);
+    set_reg(UART_LCR_OFFSET, 0x0);
     // /* Set divisor low byte */
     // set_reg(UART_DLL_OFFSET, bdiv & 0xff);
     // /* Set divisor high byte */
     // set_reg(UART_DLM_OFFSET, (bdiv >> 8) & 0xff);
     /* 8 bits, no parity, one stop bit */
     set_reg(UART_LCR_OFFSET, 0x03);
-    /* Enable FIFO */
-    set_reg(UART_FCR_OFFSET, 0x01);
-    /* No modem control DTR RTS */
-    set_reg(UART_MCR_OFFSET, 0x00);
+    /* Enable and clear FIFOs */
+    set_reg(UART_FCR_OFFSET, UART_FCR_EN | UART_FCR_RX_CLR | UART_FCR_TX_CLR);
+    /* MCR: raise OUT2. On a 16550 OUT2 gates the tri-state INTR output buffer -
+     * with OUT2=0 the RX interrupt is raised internally (IIR pending, LSR.DR=1)
+     * but never reaches the external INTR pin. Linux does this in
+     * serial8250_init_mctrl ("Most PC uarts need OUT2 raised to enable
+     * interrupts"). DTR/RTS left low. */
+    set_reg(UART_MCR_OFFSET, UART_MCR_OUT2);
     /* Clear line status */
     get_reg(UART_LSR_OFFSET);
     /* Read receive buffer */
