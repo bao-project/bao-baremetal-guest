21f91361fe2e8121df617fa7f22cea41c8511ca2
Manuel Rodríguez <manuel.cederog@gmail.com>
2026-07-02 14:31:22 +0100

    ref(tricore): use stm struct in timer module
    Signed-off-by: Manuel Rodríguez <manuel.cederog@gmail.com>

--- src/arch/tricore/timer.c
+++ src/arch/tricore/timer.c
@@ -9,35 +9,30 @@
 #include <plat.h>
 #include <irq.h>
 
-#define STM_BASE                 (0xF8800000 + ((get_cpuid()) << 18))
+#define STM ((tc_stm_t*)(0xF8800000 + ((get_cpuid()) << 18)))
 
-#define STM_CLC_OFFSET           0x000
-#define STM_OCS_OFFSET           0x004
-#define STM_ABS_OFFSET           0x020
-#define STM_CMP0_OFFSET          0x120
-#define STM_CMP1_OFFSET          0x124
-#define STM_CMCON_OFFSET         0x128
-#define STM_ICR_OFFSET           0x12C
-#define STM_ISCR_OFFSET          0x130
-#define STM_ISR_OFFSET           0x134
-#define STM_RELTIM_OFFSET        0x138
+typedef struct {
+    volatile uint32_t CLC;       /* 0x000 - 0x003 */
+    volatile uint32_t OCS;       /* 0x004 - 0x007 */
 
-#define STM_CLC_ADDR             ((STM_BASE) + (STM_CLC_OFFSET))
-#define STM_OCS_ADDR             ((STM_BASE) + (STM_OCS_OFFSET))
-#define STM_ABS_ADDR             ((STM_BASE) + (STM_ABS_OFFSET))
-#define STM_CMP0_ADDR            ((STM_BASE) + (STM_CMP0_OFFSET))
-#define STM_CMP1_ADDR            ((STM_BASE) + (STM_CMP1_OFFSET))
-#define STM_CMCON_ADDR           ((STM_BASE) + (STM_CMCON_OFFSET))
-#define STM_ICR_ADDR             ((STM_BASE) + (STM_ICR_OFFSET))
-#define STM_ISCR_ADDR            ((STM_BASE) + (STM_ISCR_OFFSET))
-#define STM_ISR_ADDR             ((STM_BASE) + (STM_ISR_OFFSET))
-#define STM_RELTIM_ADDR          ((STM_BASE) + (STM_RELTIM_OFFSET))
+    uint8_t pad0[0x020 - 0x008]; /* 0x008 - 0x01F */
+
+    volatile uint64_t ABS;       /* 0x020 - 0x027 */
+
+    uint8_t pad1[0x120 - 0x028]; /* 0x028 - 0x11F */
+
+    volatile uint32_t CMP0;      /* 0x120 - 0x123 */
+    volatile uint32_t CMP1;      /* 0x124 - 0x127 */
+    volatile uint32_t CMCON;     /* 0x128 - 0x12B */
+    volatile uint32_t ICR;       /* 0x12C - 0x12F */
+    volatile uint32_t ISCR;      /* 0x130 - 0x133 */
+    volatile uint32_t ISR;       /* 0x134 - 0x137 */
+    volatile uint64_t RELTIM;    /* 0x138 - 0x13F */
+} tc_stm_t;
 
 #define STM_CMCON_Mx_MASK        (0x1F)
 #define STM_CMCON_MSIZE0_OFFSET  (0)
 #define STM_CMCON_MSTART0_OFFSET (8)
-#define STM_CMCON_MSIZE1_OFFSET  (16)
-#define STM_CMCON_MSTART1_OFFSET (24)
 
 static bool timer_initialized = false;
 
@@ -46,46 +41,35 @@ static bool timer_initialized = false;
 
 void timer_enable()
 {
-    volatile unsigned long* ICR = STM_ICR_ADDR;
-
-    *ICR = 0x1;
+    STM->ICR = 0x1;
 }
 
 void timer_disable()
 {
-    volatile unsigned long* ICR = STM_ICR_ADDR;
-
-    *ICR = 0x0;
+    STM->ICR = 0x0;
 }
 
 uint64_t timer_get()
 {
-    volatile unsigned long long* ABS = STM_ABS_ADDR;
-    volatile unsigned long* CMP0 = STM_CMP0_ADDR;
-
-    return *ABS;
+    return STM->ABS;
 }
 
 uint64_t timer_set(uint64_t period_us)
 {
-    volatile unsigned long* CMP0 = STM_CMP0_ADDR;
-    volatile unsigned long long* ABS = STM_ABS_ADDR;
-    volatile unsigned long* CMCON = STM_CMCON_ADDR;
-
     if (!timer_initialized) {
         uint32_t size = 31;
         uint32_t start = 0;
-        *CMCON = ((MSTART_BIT0 & STM_CMCON_Mx_MASK) << STM_CMCON_MSTART0_OFFSET) |
+        STM->CMCON = ((MSTART_BIT0 & STM_CMCON_Mx_MASK) << STM_CMCON_MSTART0_OFFSET) |
             ((MSIZE_32BIT & STM_CMCON_Mx_MASK) << STM_CMCON_MSIZE0_OFFSET);
         /* Initial compare */
-        *CMP0 = (uint32_t)(*ABS + period_us);
+        STM->CMP0 = (uint32_t)(STM->ABS + period_us);
         timer_initialized = true;
     } else {
-        uint32_t current = (uint32_t)(*ABS);
+        uint32_t current = (uint32_t)(STM->ABS);
 
         uint32_t next = current + (uint32_t)period_us;
 
-        *CMP0 = next;
+        STM->CMP0 = next;
 
         return next;
     }
