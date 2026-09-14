dde4d251820540b248ad1aacd00b3db04b0cf2d5
Miguel Silva <miguelafsilva5@gmail.com>
2026-09-14 15:29:54 +0100

    feat(tc4-build): move MASTER_CPU/PLAT_CPU_NUM to arch.mk build vars
    Both were hardcoded in tc4dx's plat.h, but they're deployment choices
    (which core is master, how many cores this guest uses), not platform
    facts -- the zephyr+baremetal demo and the single-guest demo need
    different values from the same platform port. Default to the
    single-guest values (0, 6); demos needing something else pass
    MASTER_CPU=/PLAT_CPU_NUM= like any other build var. Drops start.S's own
    redundant local MASTER_CPU define now that ARCH_CPPFLAGS reaches it too.
    
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- src/platform/tc4dx/inc/plat.h
+++ src/platform/tc4dx/inc/plat.h
@@ -6,17 +6,12 @@
 #ifndef PLAT_H
 #define PLAT_H
 
-/* Total number of CPUs */
-#define PLAT_CPU_NUM     6
-
 #define PLAT_MEM_BASE    0x90100000
 #define PLAT_MEM_SIZE    0x00080000
 
 #define PLAT_CPU_FREQ    (100000000UL)  // 100 MHz
 #define CPU_TIMER_FREQ   (100000000ull) // 100 MHz
 #define PLAT_TIMER_FREQ  (CPU_TIMER_FREQ / 2)
-
-#define MASTER_CPU       0
 
 #define IRQ_SRC_NODE(id) (*((unsigned long*)(0xF4432000 + ((id) * 4))))
 
